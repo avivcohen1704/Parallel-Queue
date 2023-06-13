dcdf2aadd5541bdd10094467bc7f28d3540901ee
Aviv Cohen <62901892+avivcohen1704@users.noreply.github.com>
2023-06-13 13:24:30 +0300

    final final  queue.c

--- queue.c
+++ queue.c
@@ -1,40 +1,42 @@
 #include "queue.h"
 #include <threads.h>
 
-
-void lock_acquire(void);
-void lock_release(void);
-
 typedef struct thread_node{ // struct to represent a thread
     cnd_t cv;   
-    int channel;
     struct thread_node* next;
+    void* item;
 } thread_node;
 
 typedef struct thread_LL{ // struct to represent a linked list of threads
     thread_node* first;
     thread_node* last;
     _Atomic size_t waiting;
-    int max_channel;
 } thread_LL;
 
 typedef struct item_node{ // struct to represent an item
     void* item;
     struct item_node* next;
-    int channel;
 
 } item_node;
 
 typedef struct item_LL{ // struct to represent a linked list of items
     item_node* first;
     item_node* last;
-    int max_channel;
     _Atomic size_t size;
     _Atomic size_t visited;
 } item_LL;
 
-int init = 1;
 
+void lock_acquire(void);
+void lock_release(void);
+void print_itmes(void);
+void delete_thread_node(thread_node* node);
+void delete_item_node(item_node* node);
+
+
+
+int init = 1;
+int p = 0;
 
 item_LL* item_Q;
 mtx_t lock;
@@ -55,13 +57,11 @@ void initQueue(void){
     item_Q->last = NULL;
     item_Q->visited = 0;
     item_Q->size = 0;
-    item_Q->max_channel = 0;
 
     thread_Q = (thread_LL*) malloc(sizeof(thread_LL));
     thread_Q->first = NULL;
     thread_Q->last = NULL;
     thread_Q->waiting = 0;
-    thread_Q->max_channel = 0;
 
     init = 0;
 }
@@ -74,21 +74,18 @@ void destroyQueue(void) {
         return;
     }
 
-    if(waiting() > 0) { 
+    item_node* node = item_Q->first;
+    while(node != NULL){
+        item_node* temp = node;
+        node = node->next;
+        free(temp);
     }
 
-    item_node* temp1 = item_Q->first;
-    while(temp1 != NULL){
-
-        item_node* next1 = temp1->next;
-        free(temp1);
-        temp1 = next1;
-    }
-    thread_node* temp2 = thread_Q->first;
-    while (temp2 != NULL){
-        thread_node* next2 = temp2;
-        free(temp2);
-        temp2 = next2;        
+    thread_node* thread = thread_Q->first;
+    while(thread != NULL){
+        thread_node* temp = thread;
+        thread = thread->next;
+        free(temp);
     }
     
     free(thread_Q);
@@ -102,41 +99,40 @@ void destroyQueue(void) {
 // add an item to the queue
 void enqueue(void* item) {
     lock_acquire();
-    item_node* new_node = malloc(sizeof(item_node));
-    new_node->item = item;
-    new_node->next = NULL;
-    new_node->channel = item_Q->max_channel; // for each item sets a diff channel number that will match the right thread channel number
-    item_Q->max_channel++;
-    
-
-    if(item_Q->first == NULL){
-        item_Q->first = new_node;
-        item_Q->last = new_node;
+    if(p){
+        printf("enqueue\n");
+        print_itmes();
     }
-    else{
-        item_Q->last->next = new_node;
-        item_Q->last = new_node;
-    }
-    
     
     if(waiting() <= size()){ // if there is no waiting thread, return
+        item_node* new_node = malloc(sizeof(item_node));
+        new_node->item = item;
+        new_node->next = NULL;
+        if(item_Q->first == NULL){
+            item_Q->first = new_node;
+            item_Q->last = new_node;
+        }
+        else{
+            item_Q->last->next = new_node;
+            item_Q->last = new_node;
+        }   
         item_Q->size++;
         lock_release();
         return;
-    }
+        }
     else{ // else, wake up the first waiting thread
         item_Q->size++;
-        cnd_signal(&(thread_Q->first->cv));
 
-        thread_node* tmp2 = thread_Q->first;  // delete the thread node that was waked up, so if another enqueue will be called, it will not wake up the same thread
-        if (thread_Q->first->next == NULL) {
-            thread_Q->last = NULL;
-            thread_Q->first = NULL;
-        }
-        else{
-            thread_Q->first = thread_Q->first->next;
-        }
-        free(tmp2);
+        thread_node* thread = thread_Q->first;  // delete the thread node that was waked up, so if another enqueue will be called, it will not wake up the same thread
+        
+        thread->item = item;
+        
+        delete_thread_node(thread);
+
+
+        cnd_signal(&(thread->cv));
+
+        
 
         lock_release();
     }
@@ -148,36 +144,21 @@ void enqueue(void* item) {
 // remove an item from the queue
 void* dequeue(void){
     lock_acquire();
-
+    if(p){
+        printf("dequeue\n");
+        print_itmes();
+    }
     if(size() > waiting()){ // if there is more items than waiting threads, remove the first item
-        int channel = thread_Q->max_channel;
-        thread_Q->max_channel ++;
+
         item_node* temp = item_Q->first;
+        void* item = temp->item;
 
-        if(temp->channel == channel){ // the right one is in the first one in the Q
-            item_Q->first = item_Q->first->next;
-        }
-        else{
-            item_node* temp2 = temp->next;
-            while(temp2->channel != channel){ // search for the right item
-                temp = temp->next;
-                temp2 = temp->next;
-            }
-            if(temp2 == item_Q->last){ // the right one is the last one in the Q
-                item_Q->last = temp;
-                
-            }
-            else{
-                temp->next = temp2->next; // the right one not in the last one in the Q
-            }
-            temp = temp2;
-        }
-
+        item_Q->first = item_Q->first->next;
         if(item_Q->first == NULL){
             item_Q->last = NULL;
         }
+
         item_Q->size--;
-        void* item = temp->item;
         free(temp);
         item_Q->visited++;
 
@@ -188,44 +169,33 @@ void* dequeue(void){
         thread_node* node = (thread_node*) malloc(sizeof(thread_node)); 
         cnd_init(&(node->cv));
         node->next = NULL;
-        node->channel = thread_Q->max_channel ;
-        thread_Q->max_channel ++;
-        if(thread_Q->last == NULL){
+
+        if(thread_Q->last == NULL){ // if there are no threads
             thread_Q->last = node;
             thread_Q->first = node;
         }
-        else{ // if there is no threads
+        else{ 
             thread_Q->last->next = node;
             thread_Q->last = node;            
         }
         thread_Q->waiting++;
 
-
-
         while(size() == 0) {
             cnd_wait(&(node->cv), &lock); // wait for an item to become available
         }
-        
+        if(p){
+            printf("got signal\n");
+            print_itmes();
+        }
         cnd_destroy(&(node->cv));
-
         
-        void* res = item_Q->first->item; // removes the item from the Q
-        item_node* tmp1 = item_Q->first;
-        if(item_Q->first->next == NULL){
-            item_Q->last = NULL;
-            item_Q->first = NULL;
-        }
-        else{
-            item_Q->first = item_Q->first->next;    
-        }
-        free(tmp1);
-
-
+        void* res = node->item;
 
 
         thread_Q->waiting--;
         item_Q->visited++;
         item_Q->size--;
+        free(node);
         lock_release();
         
         return res;
@@ -236,45 +206,24 @@ void* dequeue(void){
 // try to remove an item from the queue
 bool tryDequeue(void** arg){ 
     lock_acquire();
+    if(p){
+        printf("tryDequeue\n");
+        print_itmes();
+    }
     if( size() <= waiting()) { // this if checks that there is no way for a tryDequeue to succeed, both
         lock_release();
         return false;
     }
-    int channel = thread_Q->max_channel;
-    thread_Q->max_channel ++;
-    item_node* temp = item_Q->first;
+
+    *arg = item_Q->first->item;
     
-    if(temp->channel == channel){ // the right one is in the first one in the Q
-        
-        item_Q->first = item_Q->first->next;
-        
-    }
+    item_node* node = item_Q->first;
 
-    else{
-        
-        item_node* temp2 = temp->next;
-        while(temp2->channel != channel){
-            temp = temp->next;
-            temp2 = temp->next;
-        }
-        if(temp2 == item_Q->last){ // the right one is the last one in the Q
-            item_Q->last = temp;    
-        }
-        else{
-            temp->next = temp2->next; // the right one not in the last one in the Q
-        }
-        temp = temp2;
-    }
+    delete_item_node(node);
 
-    if(item_Q->first == NULL){
-        item_Q->last = NULL;
-    }
-    item_Q->size--;
-    *arg = temp->item;
-
-    free(temp);
     item_Q->visited++;
-
+    item_Q->size--;
+    
     lock_release();
     return true;
 }
@@ -293,3 +242,66 @@ void lock_acquire(void){while(mtx_lock(&lock) < 0){}}
 
 // release the lock
 void lock_release(void){mtx_unlock(&lock);}
+
+void print_itmes(){
+    /*item_node* temp = item_Q->first;
+    printf("=========================");
+    printf("the items are:\n");
+    while(temp != NULL){
+        if(temp->item == NULL){
+            printf("stoped\n");
+            break;
+        }
+        printf("%d\n", *(int*)temp->item);
+        temp = temp->next;
+    }
+    printf("=========================");*/
+    return;
+}
+
+
+void delete_thread_node(thread_node* node){
+    if (node == thread_Q->first){
+        if (node->next == NULL){   // if there is only one thread in the Q
+            thread_Q->first = NULL;
+            thread_Q->last = NULL;
+        }
+        else{                      // if there is more than one thread in the Q
+            thread_Q->first = node->next;
+        }
+    }
+    else{
+        thread_node* prev = thread_Q->first;
+        while(prev->next != node){ // ##############################################
+            prev = prev->next;
+        }
+        if (node == thread_Q->last){  // if the node is the last one in the Q
+            thread_Q->last = prev;
+        }
+        prev->next = node->next;
+    }
+}
+
+void delete_item_node(item_node* node){
+    if (node == item_Q->first){
+        if (node->next == NULL){   // if there is only one item in the Q
+            item_Q->first = NULL;
+            item_Q->last = NULL;
+        }
+        else{                      // if there is more than one item in the Q
+            item_Q->first = node->next;
+        }
+        
+    }
+    else{
+        item_node* prev = item_Q->first;
+        while(prev->next != node){
+            prev = prev->next;
+        }
+        if (node == item_Q->last){  // if the node is the last one in the Q
+            item_Q->last = prev;
+        }
+        prev->next = node->next;
+    }
+    free(node);
+}
